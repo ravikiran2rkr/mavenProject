4070d3a02f37d37144b4fba3681960107ae132df
admin <admin@HP250G78THNEW>
2023-10-05 13:11:04 +0530

    commit first

---
+++ MavenProject/src/test/java/comit/actime/tesrscript/CustomerModule.java
@@ -0,0 +1,14 @@
+package comit.actime.tesrscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	
+	@Test
+	public void createCustomer() {
+		
+		Reporter.log("create customer");
+	}
+
+}
